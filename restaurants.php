8290a829f849f2b7c26f511b11e3ad63cbde1990
vynojusaisiri <88606296+vynojusaisiri@users.noreply.github.com>
2022-12-07 22:05:39 +0530

    Update restaurants.php

--- restaurants.php
+++ restaurants.php
@@ -34,7 +34,7 @@ session_start();
                         <ul class="nav navbar-nav">
                             <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
                             <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
-                            
+                            <li class="nav-item"> <a class="nav-link active" href="aboutus.php">Aboutus <span class="sr-only"></span></a> </li>
 							<?php
 						if(empty($_SESSION["user_id"]))
 							{
