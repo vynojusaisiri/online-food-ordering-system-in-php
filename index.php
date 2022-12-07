2df3c70a3d711340dd8106481b47f7edc66a39f9
vynojusaisiri <88606296+vynojusaisiri@users.noreply.github.com>
2022-12-07 22:01:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -36,13 +36,13 @@ session_start(); //start temp session until logout/browser closed
                         <ul class="nav navbar-nav">
                             <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
                             <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
-                            
+                            <li class="nav-item"> <a class="nav-link active" href="aboutus.php">About Us<span class="sr-only"></span></a> </li>
                            
 							<?php
 						if(empty($_SESSION["user_id"])) // if user is not login
 							{
-								echo '<li class="nav-item"><a href="login.php" class="nav-link active">login</a> </li>
-							  <li class="nav-item"><a href="registration.php" class="nav-link active">signup</a> </li>';
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
 							}
 						else
 							{
